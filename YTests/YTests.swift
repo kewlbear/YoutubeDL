a89b368857c26ec7a4e93e6bd4a6695fdd161262
Changbeom Ahn <kewlbear@gmail.com>
2020-08-04 16:22:46 +0900

    비디오 오디오 따로 받아서 병합

---
+++ YTests/YTests.swift
@@ -0,0 +1,33 @@
+//
+//  YTests.swift
+//  YTests
+//
+//  Created by 안창범 on 2020/08/03.
+//  Copyright © 2020 Jane Developer. All rights reserved.
+//
+
+import XCTest
+@testable import Y
+
+class YTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        appdele
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
--- helloworld/AppDelegate.swift
+++ helloworld/AppDelegate.swift
@@ -11,6 +11,8 @@ import Python
 import Intents
 import Photos
 
+let trace = "trace"
+
 struct Info {
     let info: PythonObject
 
@@ -55,14 +57,86 @@ struct Format {
 }
 
 class Downloader: NSObject {
+
+    enum Kind: String {
+        case complete, videoOnly, audioOnly
+        
+        var url: URL {
+            do {
+                return try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+                    .appendingPathComponent("video")
+                    .appendingPathExtension(self != .audioOnly ? "mp4" : "m4a")
+            }
+            catch {
+                print(error)
+                fatalError()
+            }
+        }
+    }
+    
     static let shared = Downloader()
     
-    func download(request: URLRequest) -> URLSessionDownloadTask {
+    func download(request: URLRequest, kind: Kind) -> URLSessionDownloadTask {
+        do {
+            try FileManager.default.removeItem(at: kind.url)
+            print(#function, "removed", kind.url.lastPathComponent)
+        }
+        catch {
+            print(#function, error)
+        }
+
         let configuration = URLSessionConfiguration.background(withIdentifier: "y")
         let session = URLSession(configuration: configuration, delegate: self, delegateQueue: nil)
         let task = session.downloadTask(with: request)
+        task.taskDescription = kind.rawValue
+        print(#function, request, trace)
         task.resume()
         return task
+    }
+    
+    func tryMerge() {
+        let videoAsset = AVAsset(url: Kind.videoOnly.url)
+        let audioAsset = AVAsset(url: Kind.audioOnly.url)
+        
+        guard let videoAssetTrack = videoAsset.tracks(withMediaType: .video).first,
+              let audioAssetTrack = audioAsset.tracks(withMediaType: .audio).first else {
+            print(#function,
+                  videoAsset.tracks(withMediaType: .video),
+                  audioAsset.tracks(withMediaType: .audio), trace)
+            return
+        }
+        
+        let composition = AVMutableComposition()
+        let videoCompositionTrack = composition.addMutableTrack(withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid)
+        let audioCompositionTrack = composition.addMutableTrack(withMediaType: .audio, preferredTrackID: kCMPersistentTrackID_Invalid)
+        
+        do {
+            try videoCompositionTrack?.insertTimeRange(CMTimeRange(start: .zero, duration: videoAssetTrack.timeRange.duration), of: videoAssetTrack, at: .zero)
+            try audioCompositionTrack?.insertTimeRange(CMTimeRange(start: .zero, duration: audioAssetTrack.timeRange.duration), of: audioAssetTrack, at: .zero)
+            print(#function, videoAssetTrack.timeRange, audioAssetTrack.timeRange, trace)
+        }
+        catch {
+            print(#function, error, trace)
+            return
+        }
+        
+        guard let session = AVAssetExportSession(asset: composition, presetName: AVAssetExportPresetPassthrough) else {
+            print(#function, "unable to init export session", trace)
+            return
+        }
+        let outputURL = Kind.videoOnly.url.deletingLastPathComponent().appendingPathComponent("output.mp4")
+        
+        try? FileManager.default.removeItem(at: outputURL)
+        
+        session.outputURL = outputURL
+        session.outputFileType = .mp4
+        print(#function, "merging...", trace)
+        session.exportAsynchronously {
+            print(#function, "finished merge", session.status.rawValue, trace)
+            if session.status == .completed {
+                self.export(outputURL)
+            }
+        }
     }
 }
 
@@ -83,28 +157,34 @@ extension Downloader: URLSessionTaskDelegate {
 }
 
 extension Downloader: URLSessionDownloadDelegate {
+    
+    fileprivate func export(_ url: URL) {
+        PHPhotoLibrary.shared().performChanges({
+            let changeRequest = PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: url)
+            //                            changeRequest.contentEditingOutput = output
+        }) { (success, error) in
+            print(#function, success, error ?? "", trace)
+            
+            let content = UNMutableNotificationContent()
+            content.body = "Download complete!"
+            let request = UNNotificationRequest(identifier: "Download", content: content, trigger: nil)
+            UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+        }
+    }
+    
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
         print(#function, session, downloadTask, location)
         
         do {
-            let url = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-                .appendingPathComponent("video")
-                .appendingPathExtension("mp4")
-
-            try? FileManager.default.removeItem(at: url)
+            let kind = Kind(rawValue: downloadTask.taskDescription ?? "") ?? .complete
             
-            try FileManager.default.moveItem(at: location, to: url)
+            try FileManager.default.moveItem(at: location, to: kind.url)
             
-            PHPhotoLibrary.shared().performChanges({
-                let changeRequest = PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: url)
-                //                            changeRequest.contentEditingOutput = output
-            }) { (success, error) in
-                print(success, error)
-            
-                let content = UNMutableNotificationContent()
-                content.body = "Download complete!"
-                let request = UNNotificationRequest(identifier: "Download", content: content, trigger: nil)
-                UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+            switch kind {
+            case .complete:
+                export(kind.url)
+            case .videoOnly,.audioOnly:
+                tryMerge()
             }
         }
         catch {
@@ -113,26 +193,37 @@ extension Downloader: URLSessionDownloadDelegate {
     }
     
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
-        print(#function, session, downloadTask, bytesWritten, totalBytesWritten, totalBytesExpectedToWrite)
+//        print(#function, session, downloadTask, bytesWritten, totalBytesWritten, totalBytesExpectedToWrite)
     }
 }
 
-func extractInfo(url: URL, completionHandler: @escaping (Info?) -> Void) {
+func extractInfo(url: URL, completionHandler: @escaping ([Format], Info?) -> Void) {
     DispatchQueue.global(qos: .userInitiated).async {
         let youtube_dl = Python.import("youtube_dl")
         
         let options: PythonObject = [
-            "format": "best[ext=mp4]",
+            "format": "bestvideo[ext=mp4],bestaudio[ext=m4a]",
             //        "outtmpl": location.path.pythonObject,
             //        "progress_hooks": [progress_hook],
             //        "nooverwrites": false,
             "nocheckcertificate": true,
+//            "update_self": true,
         ]
         
         let youtubeDL = youtube_dl.YoutubeDL(options)
         
+        print(#function, url, trace)
         let info = youtubeDL.extract_info(url.absoluteString, download: false, process: true)
-        completionHandler(Info(info: info))
+        
+        let format_selector = youtubeDL.build_format_selector(options["format"])
+        let formats_to_download = format_selector(info)
+        var formats: [Format] = []
+        for format in formats_to_download {
+            guard let dict: [String: PythonObject] = Dictionary(format) else { fatalError() }
+            formats.append(Format(format: dict))
+        }
+
+        completionHandler(formats, Info(info: info))
     }
 }
 
@@ -170,6 +261,8 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
 
+        UNUserNotificationCenter.current().delegate = self
+        
         let navigationController = window?.rootViewController as? UINavigationController
         let downloadViewController = navigationController?.topViewController as? DownloadViewController
         
@@ -195,7 +288,59 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 //            print(error)
 //        }
         
+//        if #available(iOS 11.0, *) {
+//            download(URL(string:
+////                            "https://m.youtube.com/watch?feature=youtu.be&v=fv-6WoaV6oY"
+//                        "https://youtu.be/QBT5mDJF4E8"
+//            )!)
+//        } else {
+//            // Fallback on earlier versions
+//        }
+        
         return true
+    }
+    
+    @available(iOS 11.0, *)
+    fileprivate func download(_ url: URL) {
+        extractInfo(url: url) { formats, info in
+            let video = formats.first ?? info?.format
+            guard let request = video?.urlRequest, let title = info?.title else { return }
+            
+            let progress = Progress(totalUnitCount: 100)
+            
+            let task = Downloader.shared.download(request: request, kind: formats.count > 1 ? .videoOnly : .complete)
+            
+            progress.addChild(task.progress, withPendingUnitCount: 100)
+            
+            if formats.count > 1, let request = formats.last?.urlRequest {
+                let task = Downloader.shared.download(request: request, kind: .audioOnly)
+                print("audio", task)
+                
+                progress.totalUnitCount = 200
+                progress.addChild(task.progress, withPendingUnitCount: 100)
+            }
+            
+            DispatchQueue.main.async {
+                let navigationController = self.window?.rootViewController as? UINavigationController
+                let downloadViewController = navigationController?.topViewController as? DownloadViewController
+                downloadViewController?.progressView.observedProgress =
+//                    task.progress
+                    progress
+                
+                if #available(iOS 13.0, *) {
+                    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound, .announcement, .providesAppNotificationSettings]) { (granted, error) in
+                        print(granted, error)
+                    }
+                } else {
+                    // Fallback on earlier versions
+                }
+                
+                let content = UNMutableNotificationContent()
+                content.body = #""\#(title)" 다운로드 시작!"#
+                let notificationRequest = UNNotificationRequest(identifier: "Download", content: content, trigger: nil)
+                UNUserNotificationCenter.current().add(notificationRequest, withCompletionHandler: nil)
+            }
+        }
     }
     
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
@@ -206,28 +351,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
             {
                 fatalError()
             }
-            extractInfo(url: url) { info in
-                guard let request = info?.format?.urlRequest, let title = info?.title else { return }
-                let task = Downloader.shared.download(request: request)
-                DispatchQueue.main.async {
-                    let navigationController = self.window?.rootViewController as? UINavigationController
-                    let downloadViewController = navigationController?.topViewController as? DownloadViewController
-                    downloadViewController?.progressView.observedProgress = task.progress
-
-                    if #available(iOS 13.0, *) {
-                        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound, .announcement, .providesAppNotificationSettings]) { (granted, error) in
-                            print(granted, error)
-                        }
-                    } else {
-                        // Fallback on earlier versions
-                    }
-                    
-                    let content = UNMutableNotificationContent()
-                    content.body = #""\#(title)" 다운로드 시작!"#
-                    let notificationRequest = UNNotificationRequest(identifier: "Download", content: content, trigger: nil)
-                    UNUserNotificationCenter.current().add(notificationRequest, withCompletionHandler: nil)
-                }
-            }
+            download(url)
         } else {
             // Fallback on earlier versions
         }
@@ -235,6 +359,11 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     }
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler(.alert)
+    }
+}
 
 extension PyObjectPointer {
     init<T>(_ pointer: UnsafeMutablePointer<T>) {
