6e223c3cfe9453b3886381f664b9fc307a92726d
Changbeom Ahn <kewlbear@gmail.com>
2020-11-19 15:13:41 +0900

    prepare release

---
+++ README.md
@@ -0,0 +1,25 @@
+# YoutubeDL
+
+This iOS app uses youtube-dl.
+
+## Warning
+
+This app is NOT AppStore-safe.  Historically AppStore has been removing apps downloading videos from YouTube.  This app will likely be rejected by AppStore.
+
+## Usage
+
+Run this app in a simulator.
+
+To run on a device:
+- Change code sign identity
+- Change bundle ID
+
+## Swift Packages
+
+- https://github.com/kewlbear/Python-iOS
+- https://github.com/kewlbear/FFmpeg-iOS
+- https://github.com/kewlbear/YoutubeDL-iOS
+
+## License
+
+MIT
--- YoutubeDL/AppDelegate.swift
+++ YoutubeDL/AppDelegate.swift
@@ -1,15 +1,28 @@
 //
 //  AppDelegate.swift
-//  Hello World
 //
-//  Created by 안창범 on 2020/04/20.
-//  Copyright © 2020 Jane Developer. All rights reserved.
+//  Copyright (c) 2020 Changbeom Ahn
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 import UIKit
-import Intents
-import SwiftUI
-import VideoToolbox
 import YoutubeDL
 import Resources
 
@@ -28,28 +41,12 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 
         UNUserNotificationCenter.current().delegate = self
         
-        Init()
+        SetPythonHome()
         
         _ = Downloader.shared // create URL session
         
         downloadViewController?.url = URL(string: "https://youtu.be/CM4CkVFmTds")
         
-        return true
-    }
-    
-    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-        print(userActivity.interaction ?? "no interaction?")
-        if #available(iOS 12.0, *) {
-            guard let parameter = INParameter(keyPath: \DownloadIntent.url),
-                let url = userActivity.interaction?.parameterValue(for: parameter) as? URL else
-            {
-                fatalError()
-            }
-            
-            downloadViewController?.url = url
-        } else {
-            // Fallback on earlier versions
-        }
         return true
     }
     
