56eeb162f53ed0b65a44fd6083bfb23cee9bb9a4
Changbeom Ahn <kewlbear@gmail.com>
2021-11-23 13:24:19 +0900

    fix mux

--- YoutubeDLTests/YoutubeDLTests.swift
+++ YoutubeDLTests/YoutubeDLTests.swift
@@ -8,6 +8,7 @@
 
 import XCTest
 @testable import YoutubeDL
+@testable import Y
 
 class YTests: XCTestCase {
 
@@ -19,10 +20,36 @@ class YTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testSeconds() throws {
+        XCTAssertEqual(seconds(""), 0)
+        XCTAssertEqual(seconds("1"), 1)
+        XCTAssertEqual(seconds("03"), 3)
+        XCTAssertEqual(seconds("32"), 32)
+        XCTAssertEqual(seconds("321"), 321)
+        XCTAssertEqual(seconds("3:21"), 3 * 60 + 21)
+        XCTAssertEqual(seconds("3:2"), 3 * 60 + 2)
+        XCTAssertEqual(seconds(":"), 0)
+        XCTAssertEqual(seconds(":1"), 1)
+        XCTAssertEqual(seconds("43:21"), 43 * 60 + 21)
+        XCTAssertEqual(seconds("5:43:21"), (5 * 60 + 43) * 60 + 21)
+        XCTAssertEqual(seconds("5:4:3"), (5 * 60 + 4) * 60 + 3)
         
+        XCTAssertEqual(seconds("a"), nil)
+        XCTAssertEqual(seconds("4:3:2:1"), nil)
     }
 
+    func testFormat() {
+        XCTAssertEqual(format(0), "0")
+        XCTAssertEqual(format(1), "1")
+        XCTAssertEqual(format(21), "21")
+        XCTAssertEqual(format(321), "5:21")
+        XCTAssertEqual(format(4321), "1:12:01")
+        XCTAssertEqual(format(54321), "15:05:21")
+        XCTAssertEqual(format(654321), "181:45:21")
+        
+        XCTAssertEqual(format(-1), nil)
+    }
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         measure {
